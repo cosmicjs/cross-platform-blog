685fef04d99a72d68d19999a0299d6b73013348f
Tony Spiro <tspiro@tonyspiro.com>
2016-09-21 23:03:17 -0500

    :rocket:

--- web/config/production.js
+++ web/config/production.js
@@ -1,6 +1,4 @@
 // config.js
 export default {
-  COSMIC_BUCKET: process.env.COSMIC_BUCKET,
-  COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY,
-  SMTPS_STRING: process.env.SMTPS_STRING
+  COSMIC_BUCKET: process.env.COSMIC_BUCKET
 }
